d100ba896f84dff1316c5b06ad4cb520fd6fe626
RahulRsty04 <101988127+RahulRsty04@users.noreply.github.com>
2022-05-12 08:50:30 +0530

    simple folder/file upload, drag and browse both

--- src/contexts/skynetmanager/index.tsx
+++ src/contexts/skynetmanager/index.tsx
@@ -85,9 +85,11 @@ export function SkynetManagerProvider({ children }: Props) {
       ...files.map((file) => {
         let tempPath = ''
         // prepare file's folder path but will have to check mode if directory or file otherwise below commected logic will create propblem
-        if (file.path[0] !== '/') {
+        if (file.path.indexOf('/') > -1 && file.path[0] !== '/') {
           tempPath = folderPath + '/' + file.path;
-        } 
+        } else {
+          tempPath = folderPath + file.path;
+        }
         //tempPath = folderPath + file.path; // uncomment this logic to file upload
         console.log(tempPath);
         //const absoluteFolderPath = tempPath[tempPath.length -1].join("/");
